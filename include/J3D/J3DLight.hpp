fb378dedbaaf9f29b6bdcf4fb2d96357c81ef372
Dylan Ascencio <empressglatia@gmail.com>
2023-08-08 00:01:32 -0400

    Update light struct to have defaults for all properties

--- include/J3D/J3DLight.hpp
+++ include/J3D/J3DLight.hpp
@@ -3,9 +3,9 @@
 #include <glm/glm.hpp>
 
 struct J3DLight {
-	glm::vec4 Position;
-	glm::vec4 Direction;
-	glm::vec4 Color;
-	glm::vec4 AngleAtten;
-	glm::vec4 DistAtten;
+	glm::vec4 Position = glm::vec4(0, 0, 0, 1);
+	glm::vec4 Direction = glm::vec4(0, 0, 0, 1);
+	glm::vec4 Color = glm::vec4(0, 0, 0, 0);
+	glm::vec4 AngleAtten = glm::vec4(1, 0, 0, 1);
+	glm::vec4 DistAtten = glm::vec4(1, 0, 0, 1);
 };
