d150aa9269ee149ccbf606b01b188a23bb4483bd
JoshuaMK <60854312+JoshuaMKW@users.noreply.github.com>
2025-11-25 21:22:02 -0600

    Address SetTexture inaccuracy

--- src/J3D/Material/J3DMaterialTable.cpp
+++ src/J3D/Material/J3DMaterialTable.cpp
@@ -15,65 +15,67 @@ J3DMaterialTable::~J3DMaterialTable() {
 
 }
 
-std::shared_ptr<J3DMaterial> J3DMaterialTable::GetMaterial(uint32_t idx)  {
-    return idx >= 0 && idx < mMaterials.size() ? mMaterials[idx] : std::shared_ptr<J3DMaterial>();
+std::shared_ptr<J3DMaterial> J3DMaterialTable::GetMaterial(uint32_t idx) {
+  return idx >= 0 && idx < mMaterials.size() ? mMaterials[idx] : std::shared_ptr<J3DMaterial>();
 }
 
 std::shared_ptr<J3DMaterial> J3DMaterialTable::GetMaterial(std::string name) {
-    for (std::shared_ptr<J3DMaterial>& m : mMaterials) {
-        if (m->Name == name) {
-            return m;
-        }
+  for (std::shared_ptr<J3DMaterial>& m : mMaterials) {
+    if (m->Name == name) {
+      return m;
     }
+  }
 
-    return std::shared_ptr<J3DMaterial>();
+  return std::shared_ptr<J3DMaterial>();
 }
 
 std::shared_ptr<J3DTexture> J3DMaterialTable::GetTexture(uint32_t idx) {
-    return idx >= 0 && idx < mTextures.size() ? mTextures[idx] : std::shared_ptr<J3DTexture>();
+  return idx >= 0 && idx < mTextures.size() ? mTextures[idx] : std::shared_ptr<J3DTexture>();
 }
 
 std::shared_ptr<J3DTexture> J3DMaterialTable::GetTexture(std::string name) {
-    for (std::shared_ptr<J3DTexture>& tex : mTextures) {
-        if (tex->Name == name) {
-            return tex;
-        }
+  for (std::shared_ptr<J3DTexture>& tex : mTextures) {
+    if (tex->Name == name) {
+      return tex;
     }
+  }
 
-    return std::shared_ptr<J3DTexture>();
+  return std::shared_ptr<J3DTexture>();
 }
 
 bool J3DMaterialTable::SetTexture(uint32_t idx, uint32_t width, uint32_t height, uint8_t* data, uint32_t size) {
-    if (idx >= mTextures.size()) {
-        return false;
-    }
+  if (idx >= mTextures.size()) {
+    return false;
+  }
 
-    std::shared_ptr<J3DTexture> tex = mTextures[idx];
-    tex->Width = width;
-    tex->Height = height;
+  std::shared_ptr<J3DTexture> tex = mTextures[idx];
+  tex->Width = width;
+  tex->Height = height;
 
-    // Disable mipmaps on this texture - we only support replacing textures with a single image.
-    tex->MinFilter = EGXFilterMode::Linear;
-    tex->MagFilter = EGXFilterMode::Linear;
-    tex->MipmapsEnabled = false;
-    tex->MipmapCount = 1;
-    tex->MinLOD = 0;
-    tex->MaxLOD = 0;
-    tex->LODBias = 0;
-    tex->MaxAnisotropy = EGXMaxAnisotropy::One;
+  // Disable mipmaps on this texture - we only support replacing textures with a single image.
+  tex->MinFilter = EGXFilterMode::Linear;
+  tex->MagFilter = EGXFilterMode::Linear;
+  tex->MipmapsEnabled = false;
+  tex->MipmapCount = 1;
+  tex->MinLOD = 0;
+  tex->MaxLOD = 0;
+  tex->LODBias = 0;
+  tex->MaxAnisotropy = EGXMaxAnisotropy::One;
 
-    J3DTextureLoader::InitTexture(tex);
-    J3DTextureLoader::SetTextureMipImage(tex->TexHandle, 0, width, height, data);
+  J3DTextureLoader::InitTexture(tex);
+  J3DTextureLoader::SetTextureMipImage(tex->TexHandle, 0, width, height, data);
 
-    return true;
+  return true;
 }
 
 bool J3DMaterialTable::SetTexture(std::string name, uint32_t width, uint32_t height, uint8_t* data, uint32_t size) {
-    for (uint32_t i = 0; i < mTextures.size(); i++) {
-        if (mTextures[i]->Name == name) {
-            return SetTexture(i, width, height, data, size);
-        }
+  // Since there may be multiple textures with the same name, set them all
+  // (this is equivalent to behavior found in Super Mario Sunshine).
+  bool all_good = true;
+  for (uint32_t i = 0; i < mTextures.size(); i++) {
+    if (mTextures[i]->Name == name) {
+      all_good &= SetTexture(i, width, height, data, size);
     }
-
-    return false;
+  }
+  return all_good;
 }
